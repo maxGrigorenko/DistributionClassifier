b81f39cbae39a82ae8d2756ea5a5021f3ef0c44c
maxGrigorenko <mades.gri@gmail.com>
2025-05-16 21:29:31 +0300

    chaneged algorithm of calculating dominating number

--- src/distibution_functions.py
+++ src/distibution_functions.py
@@ -1,16 +1,14 @@
 import numpy as np
 from graph_common_functions import *
 
-def generate_normal(n, sigma):
+def generate_normal(sigma, n):
     return np.random.normal(0, sigma, n)
 
-def generate_laplace(n, beta):
+def generate_laplace(beta, n):
     return np.random.laplace(0, beta, n)
 
-def avg_chars(number_of_experiments, n, graph_type, distribution, sigma=0, beta=0, k=0, d=0):
-    dominating_number_arr = []
-    delta_arr = []
-
+def avg_chars(number_of_experiments, n, graph_type, distribution, sigma=0, beta=0, k=0, d=0.0):
+    characteristic_arr = []
     for t in range(number_of_experiments):
         if distribution == "normal":
             array = generate_normal(sigma, n)
@@ -24,21 +22,16 @@ def avg_chars(number_of_experiments, n, graph_type, distribution, sigma=0, beta=
 
         if graph_type == "knn":
             g = knn_graph_constructor(array, k)
+            delta = g.compute_delta()
+            characteristic_arr.append(delta)
 
         elif graph_type == "distance":
             g = distance_graph_constructor(array, d)
+            dominating_number = g.compute_dominating_number(d=d)
+            characteristic_arr.append(dominating_number)
 
         else:
             print("Incorrect graph_type")
             exit(1)
 
-        dominating_number = g.compute_dominating_number()
-        delta = g.compute_delta()
-
-        dominating_number_arr.append(dominating_number)
-        delta_arr.append(delta)
-
-    return np.mean(dominating_number_arr), np.mean(delta_arr)
-
-
-
+    return np.mean(characteristic_arr)
--- src/graph_common_functions.py
+++ src/graph_common_functions.py
@@ -2,18 +2,24 @@ import itertools
 import time
 import random
 
+class Point:
+    def __init__(self, name, value):
+        self.name = name
+        self.value = value
+
 class Graph:
     def __init__(self, graph_dict):
-        self.graph = graph_dict
-        self.n = len(graph_dict)
+        self.graph = graph_dict  # {Point: set(Points}
         self.vertices = list(graph_dict.keys())
 
-    def compute_delta(self):
-        m = self.n
-        for v in self.vertices:
-            m = min(m, len(self.graph[v]))
+    def get_sorted_vertices(self):
+        return sorted(self.vertices, key=lambda p: p.value)
 
-        return m
+    def compute_delta(self):
+        delta = len(self.vertices)
+        for v in self.vertices:
+            delta = min(delta, len(self.graph[v]))
+        return delta
 
     def find_connected_components(self):
         visited = set()
@@ -33,123 +39,60 @@ class Graph:
                 components.append(component)
         return components
 
-    def exact_dominating_number(self, timeout=40):
-        start_time = time.time()
-        components = self.find_connected_components()
-        total = 0
+    def compute_dominating_number(self, d):
+        if not self.vertices:
+            return 0
 
-        for comp in components:
-            if time.time() - start_time > timeout:
-                return None  # Прерываем выполнение
+        sorted_vertices = self.get_sorted_vertices()
+        count = 0
+        n = len(sorted_vertices)
+        i = 0
 
-            if len(comp) == 1:
-                total += 1
-                continue
+        while i < n:
+            current_value = sorted_vertices[i].value
+            max_reach = current_value + d
 
-            subgraph = {v: self.graph[v] & set(comp) for v in comp}
-            min_size = len(comp)
-            for k in range(1, len(comp) + 1):
-                for subset in itertools.combinations(comp, k):
-                    if time.time() - start_time > timeout:
-                        return None  # Прерываем выполнение
+            # Находим последнюю вершину в интервале [current_value, max_reach)
+            j = i
+            while j < n and sorted_vertices[j].value < max_reach:
+                j += 1
 
-                    dominated = set(subset)
-                    for v in subset:
-                        dominated.update(subgraph[v])
-                    if len(dominated) == len(comp):
-                        min_size = k
-                        break
-                if min_size == k:
-                    break
-            total += min_size
-        return total
+            # Выбираем последнюю вершину интервала
+            selected = sorted_vertices[j - 1]
+            count += 1
 
-    def optimized_greedy_dominating_set(self, max_time=10):
-        dominated = set()
-        solution = []
-        vertices = set(self.vertices)
+            # Пропускаем все вершины, покрытые выбранной
+            i = j
+            while i < n and sorted_vertices[i].value < selected.value + d:
+                i += 1
 
-        # Жадный алгоритм
-        while vertices - dominated:
-            best_v = max(
-                (v for v in vertices - dominated),
-                key=lambda x: len(self.graph[x] - dominated),
-                default=None
-            )
-            if best_v is None:
-                break
-            solution.append(best_v)
-            dominated.update(self.graph[best_v])
-            dominated.add(best_v)
+        return count
 
-        # Локальный поиск
-        start_time = time.time()
-        best_solution = solution.copy()
-        best_size = len(solution)
-
-        while time.time() - start_time < max_time:
-            # Пытаемся удалить случайную вершину
-            if not best_solution:
-                break
-            candidate = random.choice(best_solution)
-            temp_solution = [v for v in best_solution if v != candidate]
-            if self._is_dominating(temp_solution):
-                best_solution = temp_solution
-                best_size -= 1
-        return best_size
-
-    def _is_dominating(self, solution):
-        dominated = set(solution)
-        for v in solution:
-            dominated.update(self.graph[v])
-        return dominated == set(self.vertices)
-
-    def compute_dominating_number(self, exact_timeout=40, greedy_time=10, verbose=True):
-        # Пытаемся найти точное решение
-        start_time = time.time()
-        exact_result = self.exact_dominating_number(exact_timeout)
-        if exact_result is not None:
-            if verbose:
-                print(f"Точное решение найдено за {time.time() - start_time:.2f} сек")
-            return exact_result
-
-        if verbose:
-            print("Точное решение не найдено. Используется жадный алгоритм...")
-        return self.optimized_greedy_dominating_set(greedy_time)
-
-class Point:
-    def __init__(self, name, value):
-        self.name = name
-        self.value = value
 
 def knn_graph_constructor(arr, k):
-    graph_dict = dict()
-    points = []
-    for i in range(len(arr)):
-        p = Point(name=i, value=arr[i])
-        points.append(p)
+    points = [Point(name=i, value=val) for i, val in enumerate(arr)]
+    graph_dict = {}
 
-    for i in range(len(points)):
-        p = points[i]
-        graph_dict.update({p.name: set(list(map(lambda x: x.name, sorted(points, key=lambda x: abs(x.value - p.value))))[1:(k+1)])})
+    for p in points:
+        sorted_points = sorted([other for other in points if other != p], key=lambda x: abs(x.value - p.value))
+        neighbors = set(sorted_points[:k])
+        graph_dict[p] = neighbors
 
     return Graph(graph_dict)
 
 
 def distance_graph_constructor(arr, d):
-    graph_dict = dict()
-    points = []
-    for i in range(len(arr)):
-        p = Point(name=i, value=arr[i])
-        points.append(p)
+    points = [Point(name=i, value=val) for i, val in enumerate(arr)]
+    graph_dict = {}
 
-    for i in range(len(points)):
-        p = points[i]
-        graph_dict.update({p.name: set([point.value for point in points if 0 < abs(point.value - p.value) < d])})
+    for p in points:
+        neighbors = set()
+        for other in points:
+            if p != other and abs(other.value - p.value) < d:
+                neighbors.add(other)
+        graph_dict[p] = neighbors
 
     return Graph(graph_dict)
-
-
 
 
 # Тестовые примеры
