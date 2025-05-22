c000d18bb88d81c28d9eb5ab3054d05a3fbc3f43
StepanSokolovsky <stepan.sokol@list.ru>
2025-05-22 22:35:08 +0300

    tests/test_dominating_number.py: debug

--- tests/test_dominating_number.py
+++ tests/test_dominating_number.py
@@ -1,5 +1,10 @@
 import unittest
-from src.graph_common_functions import Graph, distance_graph_constructor
+import sys
+import os
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '../src/normal_laplace'))
+
+from graph_common_functions import Graph, distance_graph_constructor
 
 class TestDominatingNumber(unittest.TestCase):
     def setUp(self):
